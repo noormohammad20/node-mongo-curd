ccb3775087f42a00f753f1d71fe38e06968db725
noormohammad20 <mdsaim204@gmail.com>
2022-04-21 01:39:41 +0600

    Send data to database using mongodb insertOne

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const app = express()
+const port = process.env.PORT || 5000
+
+//use middleware
+app.use(cors())
+app.use(express.json())
+
+//user: dbuser1
+//password: 4H2YqClVFczsUEKQ
+
+
+const uri = "mongodb+srv://dbuser1:4H2YqClVFczsUEKQ@cluster0.acdjr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+
+async function run() {
+    try {
+        await client.connect()
+        const userCollection = client.db("foodExpress").collection("user")
+        const user = { name: 'mohona nodi', email: 'nodi@gmail.com' }
+        const result = await userCollection.insertOne(user)
+        console.log(`User inserted with id: ${result.insertedId}`)
+    }
+    finally {
+        // await client.close()
+    }
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send("Running My Node CURD Server")
+})
+
+app.listen(port, () => {
+    console.log("CURD Server Is Running", port)
+})
